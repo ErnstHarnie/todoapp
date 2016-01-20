bf0d31dc87b37a17e44130faef79f9f1b64d13bc
ernstharnie <ernst_harnie@hotmail.com>
2016-01-20 16:55:11 +0100

    Edit added

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,23 +1,48 @@
 class TodoController < ApplicationController
   def index
+      url = request.original_url
+	uri = URI::parse(url)
+	id = uri.path.split('/')[0]
+	params = CGI::parse(uri.query)
+	
+	render :text => params.inspect  # print to screen
+
+  #result = Todo.All
   end
   
   def new
   
+    url = request.original_url
+	uri = URI::parse(url)
+	id = uri.path.split('/')[0]
+	params = CGI::parse(uri.query)
+	
+
+	render :text => params.inspect # print to screen
+	#params.save
   end
   
   def create
-	   require 'net/http'
-		source = 'http://localhost:3000/todo/new/'
-		resp = Net::HTTP.get_response(URI.parse(source))
-		data = resp.body
-		result = JSON.parse(data)
-		
-		
-		#@todo = Todo.new(params[:todo])
+render plain: params[:todo].inspect
+  end
+  
+  def edit
+  @todo = Todo.find(params[:id])
+end
+
+def update
+  @todo = Todo.find(params[:id])
  
-		#@todo.save
-		#redirect_to @todo
+  if @todo.update(todo_params)
+    redirect_to @todo
+  else
+    render 'edit'
+  end
+end
+  
+private
+  def todo_params
+    params.require(:todo).permit(:startdate, :enddate, :description, :priority, :status)
   end
   
 end
